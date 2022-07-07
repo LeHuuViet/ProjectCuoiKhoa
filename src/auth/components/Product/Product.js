1500c28238552a712998649b32bd7d605db38bf5
Le Huu Viet <lhv.nk53@gmail.com>
2022-07-07 17:50:55 +0700

    display userlist table

--- src/auth/components/Product/Product.js
+++ src/auth/components/Product/Product.js
@@ -1,4 +1,3 @@
-import { faTruckField } from "@fortawesome/free-solid-svg-icons";
 import React, { useEffect, useState, useDispatch } from "react";
 import DataTable from "react-data-table-component";
 import ReactPaginate from "react-paginate";
@@ -9,120 +8,145 @@ function Product() {
   const [checkAll, setCheckAll] = useState(false);
   const [data, setData] = useState([]);
   const [page, setPage] = useState(1);
-  const [pageCount, setPageCount] = useState(0)
-  const [itemsPerPage, setItemsPerPage] = useState(10)
+  const [pageCount, setPageCount] = useState(0);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
   const [totalItems, setTotalItems] = useState(0);
 
-  
   useEffect(() => {
-    const datas = fetch('https://api.gearfocus.div4.pgtest.co/api/products/list')
-    .then(response => response.json())
-    .then(data => setData(data));
-    setTotalItems(data.recordsFiltered)
+    const datas = fetch(
+      "https://api.gearfocus.div4.pgtest.co/api/products/list", {method: 'POST', headers: {Authorization: '9.5a8eefea2a1299f87e8e1a74994827840debf897a605c603444091fa519da275'}}
+    )
+      .then((response) => response.json())
+      .then((data) => setData(data));
+    setTotalItems(data.recordsFiltered);
     const pc = data.recordsFiltered / itemsPerPage;
-    setPageCount(Math.round(pc))
+    setPageCount(Math.round(pc));
   }, [page, itemsPerPage]);
 
   const handleChangePage = (e) => {
     setPage(e.selected + 1);
-  }
+  };
 
   const handleCheckAll = () => {
-    var checkboxes = document.getElementsByName('checkbox');
+    var checkboxes = document.getElementsByName("checkbox");
     for (var i = 0; i < checkboxes.length; i++) {
       checkboxes[i].checked = true;
     }
-    setCheckAll(false)
-  }
+    setCheckAll(false);
+  };
 
   const handleUnCheckAll = () => {
-    var checkboxes = document.getElementsByName('checkbox')
+    var checkboxes = document.getElementsByName("checkbox");
     for (var i = 0; i < checkboxes.length; i++) {
       checkboxes[i].checked = false;
     }
-    setCheckAll(true)
-  }
+    setCheckAll(true);
+  };
 
   const handleCheckAndUnCheck = (checkAll) => {
     if (checkAll === true) handleCheckAll();
     else handleUnCheckAll();
-  } 
-
+  };
 
   const columns = [
     {
-      name: <input name='checkbox' type={'checkbox'} onClick={() => handleCheckAndUnCheck(checkAll)}/>,
-      selector: row => <input name='checkbox' type={'checkbox'}/>
+      name: (
+        <input
+          name="checkbox"
+          type={"checkbox"}
+          onClick={() => handleCheckAndUnCheck(checkAll)}
+        />
+      ),
+      selector: (row) => <input name="checkbox" type={"checkbox"} />,
+      width: '50px'
     },
     {
-      name: 'SKU',
-      selector: row => row.sku
+      name: "SKU",
+      selector: (row) => row.sku,
+      width: '150px'
     },
     {
-      name: 'Name',
-      selector: row => <a href='' style={{width: '300px'}}>{row.name}</a>
+      name: "Name",
+      selector: (row) => (
+        <a href="" style={{color: '#007bff'}}>
+          {row.name}
+        </a>
+      ),
+      width: '300px'
     },
     {
-      name: 'Category',
-      selector: row => row.category
-    },
-    {
-      name: 'Price',
-      selector: row => <input value={row.price}/>
+      name: "Category",
+      selector: (row) => row.category,
+      width: '200px'
 
     },
     {
-      name: 'Instock',
-      selector: row => <input value={row.amount}/>
+      name: "Price",
+      selector: (row) => <input value={row.price} />,
+      width: '100px'
     },
     {
-      name: 'Vendor',
-      selector: row => <a href="">{row.vendor}</a>
+      name: "Instock",
+      selector: (row) => <input value={row.amount} />,
+      width: '100px'
     },
     {
-      name: 'Arrival Date',
-      selector: row => moment(row.arrivalDate).format('ll')
+      name: "Vendor",
+      selector: (row) => <a href="" style={{color: '#007bff'}}>{row.vendor}</a>,
+      width: '200px'
     },
     {
-      name: '',
-      selector: row => <button className="product-delete">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM394.8 466.1C393.2 492.3 372.3 512 346.9 512H101.1C75.75 512 54.77 492.3 53.19 466.1L31.1 128H416L394.8 466.1z"/></svg>
-      </button>
+      name: "Arrival Date",
+      selector: (row) => moment.unix(Number.parseInt(row.arrivalDate)).format("ll"),
+      width: '200px'
     },
-    
-  ]
+    {
+      name: "",
+      selector: (row) => (
+        <button className="product-delete">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <path d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM394.8 466.1C393.2 492.3 372.3 512 346.9 512H101.1C75.75 512 54.77 492.3 53.19 466.1L31.1 128H416L394.8 466.1z" />
+          </svg>
+        </button>
+      ),
+    },
+  ];
 
   return (
     <div className="Products">
       <div>
         <h2 className="products-title">Products</h2>
         <div className="product-search-form">
-          <form>
+          <form className="border">
             <ul className="products-search-1 d-flex">
-              <li style={{width: '50%', marginRight: '20px'}}>
+              <li style={{ width: "50%", marginRight: "20px" }}>
                 <div>
                   <input
-                    className="products-search-input"
+                    className="products-search-input border"
                     placeholder="Search keywords"
                   />
                 </div>
               </li>
-              <li style={{width: '25%', marginRight: '20px'}}>
+              <li style={{ width: "25%", marginRight: "20px" }}>
                 <div>
-                  <select className="products-search-select"></select>
+                  <select className="products-select border"></select>
                 </div>
               </li>
-              <li style={{width: '20%', marginRight: '20px'}}>
+              <li style={{ width: "20%", marginRight: "20px" }}>
                 <div>
-                  <select className="products-search-select" id='inventory' defaultValue={'Any stock status'}>
-                    <option value='all'>Any stock status</option>
-                    <option value='in'>In stock</option>
-                    <option value='low'>Low stock</option>
-                    <option value='out'>SOLD</option>
+                  <select
+                    className="products-select border"
+                    id="inventory"
+                    defaultValue={"Any stock status"}
+                  >
+                    <option value="all">Any stock status</option>
+                    <option value="in">In stock</option>
+                    <option value="low">Low stock</option>
+                    <option value="out">SOLD</option>
                   </select>
                 </div>
               </li>
-              <li style={{width: '5%', marginRight: '20px'}}>
+              <li style={{ width: "5%", marginRight: "20px" }}>
                 <div>
                   <button className="products-search-button">Search</button>
                 </div>
@@ -130,11 +154,11 @@ function Product() {
             </ul>
             <div className="line-1"></div>
             <ul className="products-search-2 d-flex">
-              <li className="d-flex" style={{marginRight: '40px'}}>
+              <li className="d-flex" style={{ marginRight: "40px" }}>
                 <label>Search in:</label>
-                <ul style={{padding: '0px'}}>
+                <ul style={{ padding: "0px" }}>
                   <li>
-                    <input type={"checkbox"}/>
+                    <input type={"checkbox"} />
                     <label>Name</label>
                   </li>
                   <li>
@@ -147,59 +171,67 @@ function Product() {
                   </li>
                 </ul>
               </li>
-              <li className="d-flex" style={{marginRight: '40px'}}>
+              <li className="d-flex" style={{ marginRight: "40px" }}>
                 <label>Availability</label>
-                <select className="products-search-select" defaultValue={'Any availability status'}>
-                    <option value='all'>Any availability status</option>
-                    <option value='1'>Only enabled</option>
-                    <option value='0'>Only disabled</option>
+                <select
+                  className="products-select border"
+                  defaultValue={"Any availability status"}
+                >
+                  <option value="all">Any availability status</option>
+                  <option value="1">Only enabled</option>
+                  <option value="0">Only disabled</option>
                 </select>
               </li>
-              <li className="d-flex" style={{marginRight: '40px'}}>
+              <li className="d-flex" style={{ marginRight: "40px" }}>
                 <label>Vendor</label>
-                <input className="products-search-input" />
+                <input className="products-search-input border" />
               </li>
             </ul>
           </form>
         </div>
       </div>
-          <div>
-            <button className='products-search-button'>Add product</button>
-          </div>
-          <div className="products-table">
-            <DataTable 
-            keyField="id"
-            columns={columns}
-            data={data.data}
-            pagination={true}
-              />
-            <div className="pagination-bar">
-                <ReactPaginate
-                    breakLabel="..."
-                    nextLabel=">>"
-                    onPageChange={handleChangePage}
-                    pageRangeDisplayed={2}
-                    marginPagesDisplayed={1}
-                    pageCount={pageCount}
-                    previousLabel="<<"
-                    renderOnZeroPageCount={null}
-                    containerClassName="pagination"
-                    pageLinkClassName="page-num"
-                    previousLinkClassName="page-num"
-                    nextLinkClassName="page-num"
-                    activeLinkClassName='active'
-                />
-                <div>{totalItems} items</div>
-                <select className="pagiSelect" onChange={(e) => setItemsPerPage(e.target.value)} defaultValue={25}>
-                    <option value="10">10</option>
-                    <option value="25">25</option>
-                    <option value="50">50</option>
-                    <option value="75">75</option>
-                    <option value="100">100</option>
-                </select>
-            </div>
-          </div>
-          </div>
-  )};
+      <div>
+        <button className="products-search-button">Add product</button>
+      </div>
+      <div className="products-table border">
+        <DataTable
+          keyField="id"
+          columns={columns}
+          data={data.data}
+          pagination={true}
+        />
+        <div className="pagination-bar">
+          <ReactPaginate
+            breakLabel="..."
+            nextLabel=">>"
+            onPageChange={handleChangePage}
+            pageRangeDisplayed={2}
+            marginPagesDisplayed={1}
+            pageCount={pageCount}
+            previousLabel="<<"
+            renderOnZeroPageCount={null}
+            containerClassName="pagination"
+            pageLinkClassName="page-num"
+            previousLinkClassName="page-num"
+            nextLinkClassName="page-num"
+            activeLinkClassName="active"
+          />
+          <div>{totalItems} items</div>
+          <select
+            className="pagiSelect"
+            onChange={(e) => setItemsPerPage(e.target.value)}
+            defaultValue={25}
+          >
+            <option value="10">10</option>
+            <option value="25">25</option>
+            <option value="50">50</option>
+            <option value="75">75</option>
+            <option value="100">100</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  );
+}
 
-  export default Product;
+export default Product;
--- src/auth/components/UserList/UserList.js
+++ src/auth/components/UserList/UserList.js
@@ -1,75 +1,183 @@
+import "./UserList.scss";
+import DataTable from "react-data-table-component";
+import { useEffect, useState } from "react";
+import moment from "moment";
+
 function UserList() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const data = fetch(
+      "https://api.gearfocus.div4.pgtest.co/apiAdmin/users/list",
+      {
+        method: "POST",
+        headers: {
+          Authorization:
+            "9.5a8eefea2a1299f87e8e1a74994827840debf897a605c603444091fa519da275",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+
+  const columns = [
+    {
+      name: <input type={"checkbox"} />,
+      selector: (row) => <input type={"checkbox"} />,
+      width: "50px",
+    },
+    {
+      name: "Login/Email",
+      selector: (row) => (
+        <div className="d-flex" style={{ flexDirection: "column" }}>
+          <a href="" style={{ color: "#007bff" }}>
+            {row.vendor}
+          </a>
+          <label>{row.storeName}</label>
+        </div>
+      ),
+      width: "300px",
+    },
+    {
+      name: "Name",
+      selector: (row) => {
+        if (row.fistName == null && row.lastName == null) return "";
+        else return row.fistName + " " + row.lastName;
+      },
+      width: "200px",
+    },
+
+    {
+      name: "Access Level",
+      selector: (row) => row.access_level,
+      width: "150px",
+    },
+    {
+      name: "Products",
+      selector: (row) => row.product,
+    },
+    {
+      name: "Orders",
+      selector: (row) => row.order.order_as_buyer,
+    },
+    {
+      name: "Created",
+      selector: (row) =>
+        moment.unix(Number.parseInt(row.created)).format("lll"),
+      width: "200px",
+    },
+    {
+      name: "Last Login",
+      selector: (row) =>
+        moment.unix(Number.parseInt(row.last_login)).format("lll"),
+      width: "200px",
+    },
+    {
+      name: "",
+      selector: (row) => <button className="userlist-delete">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+        <path d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM394.8 466.1C393.2 492.3 372.3 512 346.9 512H101.1C75.75 512 54.77 492.3 53.19 466.1L31.1 128H416L394.8 466.1z" />
+      </svg>
+    </button>,
+    },
+  ];
   return (
     <div className="UserList">
-      <div>
-        <h2 className="userlist-title">Search for user</h2>
-        <div className="userlist-search-from">
-          <form>
-            <ul className="userlist-search-1 d-flex">
-              <li>
-                <input placeholder="Search keywords" />
-              </li>
-              <li>
-                <select defaultValue={"All memberships"}>
-                  <option>General</option>
-                  <option>General</option>
-                </select>
-              </li>
-              <li>
-                <select>All user types</select>
-              </li>
-              <li>
-                <select></select>
-              </li>
-              <li>
+      <div style={{ marginBottom: "40px" }}>
+        <h2 className="userlist-title">Search for users</h2>
+        <form className="border">
+          <ul className="userlist-search-1 d-flex">
+            <li style={{ width: "50%", marginRight: "20px" }}>
+              <input
+                className="userlist-search-input border width100"
+                placeholder="Search keywords"
+              />
+            </li>
+            <li style={{ width: "50%", marginRight: "20px" }}>
+              <select
+                className="userlist-select border width100"
+                defaultValue={"All memberships"}
+              >
+                <option>General</option>
+                <option>General</option>
+              </select>
+            </li>
+            <li style={{ width: "50%", marginRight: "20px" }}>
+              <select className="userlist-select border width100">
+                All user types
+              </select>
+            </li>
+            <li style={{ width: "35%", marginRight: "20px" }}>
+              <select className="userlist-select border width100"></select>
+            </li>
+            <li>
+              <div>
+                <button className="userlist-button width100">
+                  Search
+                </button>
+              </div>
+            </li>
+          </ul>
+          <div className="line-1"></div>
+          <ul className="userlist-search-2 d-flex">
+            <li className="userlist-search-2-1">
+              <ul>
+                <li className="d-flex space-between">
+                  <label>Country</label>
+                  <select
+                    className="userlist-select border"
+                    style={{ width: "100%" }}
+                  ></select>
+                </li>
+                <li className="d-flex space-between">
+                  <label>State</label>
+                  <input className="userlist-search-input border" />
+                </li>
+                <li className="d-flex space-between">
+                  <label>Address</label>
+                  <input className="userlist-search-input border" />
+                </li>
+                <li className="d-flex space-between">
+                  <label>Phone</label>
+                  <input className="userlist-search-input border" />
+                </li>
+              </ul>
+            </li>
+            <li className="userlist-search-2-2">
+              <div className="d-flex">
+                <label>User activity</label>
                 <div>
-                  <button>Search</button>
+                  <ul className="d-flex">
+                    <li>
+                      <input type={"checkbox"} />
+                      <label>Register</label>
+                    </li>
+                    <li>
+                      <input type={"checkbox"} />
+                      <label>Last Logged in</label>
+                    </li>
+                  </ul>
+                  <input
+                    className="userlist-search-input border"
+                    placeholder="Enter date range"
+                    style={{ width: "300px", marginTop: "10px" }}
+                  />
                 </div>
-              </li>
-            </ul>
-            <div className="line-1"></div>
-            <ul className="userlist-search-2 d-flex">
-              <li>
-                <ul>
-                  <li>
-                    <label>Country</label>
-                    <select></select>
-                  </li>
-                  <li>
-                    <label>State</label>
-                    <input />
-                  </li>
-                  <li>
-                    <label>Address</label>
-                    <input />
-                  </li>
-                  <li>
-                    <label>Phone</label>
-                    <input />
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <div className="d-flex">
-                  <label>User activity</label>
-                  <div>
-                    <ul>
-                      <li>
-                        <input type={"checkbox"} />
-                        <label>Register</label>
-                      </li>
-                      <li>
-                        <input type={"checkbox"} />
-                        <label>Last Logged in</label>
-                      </li>
-                    </ul>
-                    <input />
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </form>
-        </div>
+              </div>
+            </li>
+          </ul>
+        </form>
+      </div>
+      <div>
+        <button className="userlist-button">Add User</button>
+      </div>
+      <div className="userlist-table">
+        <DataTable
+          keyField="id"
+          columns={columns}
+          data={data.data}
+          pagination={true}
+        />
       </div>
     </div>
   );
